518237cb73c06927bd8c8b7148efa0d03639c211
bdowningdev <bdowningdev@gmail.com>
2024-12-23 16:18:44 -0700

    created cirleshape.py and player.py. modified main and constants

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-
+        dt = clock.tick(60) / 1000
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
