e6452a1768640e6fa8f152733f6ad3b5bffac4c1
bdowningdev <bdowningdev@gmail.com>
2024-12-23 18:30:34 -0700

    updated main

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -15,8 +18,12 @@ def main():
                 return
         screen.fill("black")
         dt = clock.tick(60) / 1000
-        player.update(dt)
-        player.draw(screen)
+        for thing in updatable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+        #player.update(dt)
+        #player.draw(screen)
         pygame.display.flip()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
